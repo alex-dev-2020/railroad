935234186561ffb40e8c900c455dee14773a62b1
Alex Dev <alex_dev_2020@mail.ru>
2020-02-17 22:43:11 +1000

    Created route.rb

---
+++ route.rb
@@ -0,0 +1,19 @@
+class Route
+  attr_reader :stations
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    if (station != @stations.first) && (station != @stations.last)
+      @stations.delete(station)
+    else
+      puts 'Конечные точки маршрута удалить нельзя!'
+    end
+  end
+end
