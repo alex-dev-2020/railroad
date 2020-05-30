0687e52c20d476fd4b988b900e03da5156d9abf7
Alex Dev <alex_dev_2020@mail.ru>
2020-05-31 00:19:51 +1000

    Trailing whitespace fix

--- Lesson_8/cargo_wagon.rb
+++ Lesson_8/cargo_wagon.rb
@@ -1,5 +1,5 @@
 require_relative 'wagon'
-# lines below included only for test purpose 
+# lines below included only for test purpose
 require_relative 'instance_counter'
 require_relative 'valid'
 
--- Lesson_8/pass_train.rb
+++ Lesson_8/pass_train.rb
@@ -3,9 +3,9 @@ class PassTrain < Train
   def type
     :pass
   end
-  
+
   def add_wagon(wagon)
     super(wagon) if wagon.is_a?(PassWagon)
   end
-  
+
 end
